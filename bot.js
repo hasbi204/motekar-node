76569af23d4fbed548111d00faf880cfa3d3380b
Hasbi <muhammadhasbi204@gmail.com>
2026-03-28 01:32:03 +0700

    update

--- bot.js
+++ bot.js
@@ -1,4 +1,14 @@
 require('dns').setDefaultResultOrder('ipv4first');
+
+process.on('unhandledRejection', (err) => {
+  logger.error('UNHANDLED REJECTION: ' + err.message);
+});
+
+process.on('uncaughtException', (err) => {
+  logger.error('UNCAUGHT EXCEPTION: ' + err.message);
+});
+
+const logger = require('./logger');
 const { Telegraf } = require('telegraf');
 const bot = new Telegraf('836397615:AAH2Gvo_RkYp6aBl9XsNKgVtx0eAJ2hebxk');
 
@@ -8,4 +18,5 @@ require('./commands/lang')(bot);
 
 bot.launch();
 
-console.log('Bot jalan...');
+console.log('Bot starting...');
+logger.info('Bot starting...');
