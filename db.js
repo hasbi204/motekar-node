d62885b0a5f18f836e742c893bd9715975e63b29
Hasbi <muhammadhasbi204@gmail.com>
2026-03-27 23:25:54 +0700

    update

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const db = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'pD@!MYMuT2r2dbK',
   database: 'motekardata',
 });
 
