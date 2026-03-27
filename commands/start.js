05072607ac422acf5c280eacf3c4f2f129f0e40d
Hasbi <muhammadhasbi204@gmail.com>
2026-03-28 02:40:30 +0700

    update

--- commands/start.js
+++ commands/start.js
@@ -3,6 +3,7 @@ const { getLang } = require('../lang');
 module.exports = (bot) => {
   bot.start(async (ctx) => {
     const lang = await getLang(ctx.from.id, ctx.from.language_code);
-    ctx.reply(lang.welcome);
+    const name = ctx.from.first_name || 'User';
+    ctx.reply(`${lang.welcome}\n\nHalo ${name} 👋`);
   });
 };
