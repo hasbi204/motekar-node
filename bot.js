701646258ac95ae4851e8e96deee05409a592b5c
Hasbi <muhammadhasbi204@gmail.com>
2026-03-28 00:47:42 +0700

    update

--- bot.js
+++ bot.js
@@ -1,18 +1,10 @@
 require('dns').setDefaultResultOrder('ipv4first');
-
 const { Telegraf } = require('telegraf');
-
 const bot = new Telegraf('836397615:AAH2Gvo_RkYp6aBl9XsNKgVtx0eAJ2hebxk');
 
-// load commands
 require('./commands/start')(bot);
 require('./commands/help')(bot);
 require('./commands/lang')(bot);
-
-bot.on('text', (ctx) => {
-  console.log('MASUK PESAN:', ctx.message.text);
-  ctx.reply('Test reply jalan');
-});
 
 bot.launch();
 
