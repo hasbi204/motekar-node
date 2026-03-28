75bc4c85a2443287d20baef8a5e569dbe11a2554
Hasbi <muhammadhasbi204@gmail.com>
2026-03-28 12:59:56 +0700

    update

--- commands/lang.js
+++ commands/lang.js
@@ -1,19 +1,45 @@
 const { setUserLang } = require('../storage/users');
 const { getLang } = require('../lang');
+const { Markup } = require('telegraf');
 
 module.exports = (bot) => {
   bot.command('lang', async (ctx) => {
     const lang = await getLang(ctx.from.id, ctx.from.language_code);
-    ctx.reply(lang.choose);
+    ctx.reply(lang.choose,
+      Markup.inlineKeyboard([
+        [
+          Markup.button.callback('🇮🇩 Indonesia', 'lang_id'),
+          Markup.button.callback('🇺🇸 English', 'lang_en'),
+        ],
+      ])
+    );
   });
 
-  bot.command('lang_id', async (ctx) => {
+  bot.action('lang_id', async (ctx) => {
     await setUserLang(ctx.from.id, 'id');
-    ctx.reply('Bahasa diubah ke Indonesia 🇮🇩');
+    const lang = await getLang(ctx.from.id, 'id');
+    await ctx.editMessageText(
+      lang.choose,
+      Markup.inlineKeyboard([
+        [
+          Markup.button.callback('🇮🇩 Indonesia', 'lang_id'),
+          Markup.button.callback('🇺🇸 English', 'lang_en'),
+        ],
+      ])
+    );
   });
 
-  bot.command('lang_en', async (ctx) => {
+  bot.action('lang_en', async (ctx) => {
     await setUserLang(ctx.from.id, 'en');
-    ctx.reply('Language changed to English 🇺🇸');
+    const lang = await getLang(ctx.from.id, 'en');
+    await ctx.editMessageText(
+      lang.choose,
+      Markup.inlineKeyboard([
+        [
+          Markup.button.callback('🇮🇩 Indonesian', 'lang_id'),
+          Markup.button.callback('🇺🇸 English', 'lang_en'),
+        ],
+      ])
+    );
   });
 };
