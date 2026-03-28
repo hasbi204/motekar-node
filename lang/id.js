3be4e3bf6ec489c0d5f3cd615b42b3646dd403d5
Hasbi <muhammadhasbi204@gmail.com>
2026-03-28 12:41:59 +0700

    update

--- lang/id.js
+++ lang/id.js
@@ -1,5 +1,5 @@
 module.exports = {
   welcome: "Selamat datang di Bot Motekar 🚀\n\nJika memerlukan bantuan ketik /help",
   help: "📖 Daftar Perintah:\n/start - Mulai bot\n/help - Bantuan\n/lang - Pilih Bahasa",
-  choose: "Silahkan Pilih bahasa:\n\n1. Indonesia (/lang_id)\n2. English (/lang_en)"
+  choose: "Silahkan Pilih bahasa:\n\n1. Indonesia (/lang_id)\n2. Inggris (/lang_en)"
 };
