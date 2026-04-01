7bafdc8b62f2f22b6bb285235837bfd78cff228e
Hasbi <muhammadhasbi204@gmail.com>
2026-04-02 00:28:58 +0700

    update

--- ws.js
+++ ws.js
@@ -48,21 +48,38 @@ function startWS(bot) {
 }, 10 * 60 * 1000); // 10 menit
 
   const ws = new WebSocket(ALCHEMY_WS);
-  ws.on('open', () => {
+  ws.on('open', () => {    
     console.log('✅ WebSocket connected');
-    ws.send(
-      JSON.stringify({
-        jsonrpc: '2.0',
-        id: 1,
-        method: 'eth_subscribe',
-        params: ['newPendingTransactions'],
-      })
-    );
+
+    // 1️⃣ Subscribe ETH pending tx
+    ws.send(JSON.stringify({
+      jsonrpc: '2.0',
+      id: 1,
+      method: 'eth_subscribe',
+      params: ['newPendingTransactions'],
+    }));
+
+    // 2️⃣ Subscribe ERC20 logs
+    ws.send(JSON.stringify({
+      jsonrpc: '2.0',
+      id: 2,
+      method: 'eth_subscribe',
+      params: [
+        'logs',
+        {
+          topics: [
+            '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55aeea4a7f4f'
+          ]
+        }
+      ],
+    }));
   });
 
   ws.on('message', async (data) => {
     const msg = JSON.parse(data.toString());
-    if (msg.method === 'eth_subscription') {
+
+    // Handle transaksi ETH
+    if (msg.method === 'eth_subscription' && typeof msg.params.result === 'string') {
       const txHash = msg.params.result;
       sendRPC(ws, 'eth_getTransactionByHash', [txHash], Date.now());
       return;
@@ -109,7 +126,43 @@ Tx: https://etherscan.io/tx/${tx.hash}`,
 });
       
       sentTx.add(tx.hash);
+      return;
     }
+
+    // Handle transaksi ERC20
+    if (msg.method === 'eth_subscription' && msg.params?.result?.topics) {
+      const log = msg.params.result;
+      if (!log.topics || log.topics.length < 3) return;
+      const from = '0x' + log.topics[1].slice(26);
+      const to = '0x' + log.topics[2].slice(26);
+      const matched = trackedWallets.find(w => w.address === from.toLowerCase() || w.address === to.toLowerCase());
+      if (!matched) return;
+
+      const isIncoming = to.toLowerCase() === matched.address;
+      const rawValue = BigInt(log.data);
+      const amount = Number(rawValue); // nanti kita adjust
+      const type = isIncoming ? '📥 Token Masuk' : '📤 Token Keluar';
+      console.log('ERC20 TX:', { from, to, amount });
+
+      if (sentTx.has(log.transactionHash)) return;
+
+  await bot.telegram.sendMessage(
+    matched.user_id,
+    `${type}
+${matched.name}
+
+From: ${from}
+To: ${to}
+
+Amount: ${amount}
+Tx: https://etherscan.io/tx/${log.transactionHash}`);
+
+  sentTx.add(log.transactionHash);
+  return;
+}
+
+
+
   });
 
   ws.on('error', (err) => {
