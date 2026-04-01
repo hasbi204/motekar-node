cd006b5551f74232c225b764908f6c7a3e98b7ef
Hasbi <muhammadhasbi204@gmail.com>
2026-04-01 20:29:46 +0700

    update

--- ws.js
+++ ws.js
@@ -1,14 +1,21 @@
 const WebSocket = require('ws');
-
 const ALCHEMY_WS = process.env.ALCHEMY_WS;
+
+function sendRPC(ws, method, params, id = 2) {
+  ws.send(
+    JSON.stringify({
+      jsonrpc: '2.0',
+      id,
+      method,
+      params,
+    })
+  );
+}
 
 function startWS() {
   const ws = new WebSocket(ALCHEMY_WS);
-
   ws.on('open', () => {
     console.log('✅ WebSocket connected');
-
-    // subscribe pending tx
     ws.send(
       JSON.stringify({
         jsonrpc: '2.0',
@@ -21,18 +28,24 @@ function startWS() {
 
   ws.on('message', (data) => {
     const msg = JSON.parse(data.toString());
-
     if (msg.method === 'eth_subscription') {
       const txHash = msg.params.result;
-
-      // console.log('TX HASH:', txHash);
+      sendRPC(ws, 'eth_getTransactionByHash', [txHash], Date.now());
+      return;
+    }
+    if (msg.result && msg.result.hash) {
+      const tx = msg.result;
+      console.log('TX DETAIL:', {
+        from: tx.from,
+        to: tx.to,
+        value: tx.value,
+      });
     }
   });
 
   ws.on('error', (err) => {
     console.error('WS Error:', err.message);
   });
-
   ws.on('close', () => {
     console.log('❌ WS disconnected');
   });
