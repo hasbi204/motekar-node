408768a9d54d58b4015d2844dfd451a776d6345a
Hasbi <muhammadhasbi204@gmail.com>
2026-04-02 01:09:29 +0700

    update

--- ws.js
+++ ws.js
@@ -68,7 +68,8 @@ function startWS(bot) {
         'logs',
         {
           topics: [
-            '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55aeea4a7f4f'
+            '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55aeea4a7f4f',
+            '0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef'
           ]
         }
       ],
